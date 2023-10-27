da9965053c37f67f3216224a40895bd8071722db
hxmxad <148150297+hxmxad@users.noreply.github.com>
2023-10-27 13:56:42 +0000

    c practice

--- hello.c
+++ hello.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 int main() {
-    printf("Hello world");
+    float radius;
+    float area;
+    printf("what is the radius of the circle?\n");
+    scanf("%f", &radius);
+
+    area = radius * radius * 3.14;
+
+printf("%f", area);
 
 return 0;
 }
---
+++ loops.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int i;
+    for(i = 1; i <= 100; i = i + 1)
+    {
+        printf("%d\n", i);
+    }
+
+    printf("\n\n\n");
+
+    int a;
+    a = 1;
+    while(a <= 100)
+    {
+    printf("%d\n", a);
+    a = a + 1;
+    }
+
+return 0;
+}
